0fc1ad5f1fa71dbce4e739d9ce464130914753a3
jordandong <jordandong@gmail.com>
2014-03-25 15:29:31 -0700

    20140325

---
+++ KMP.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void cal_next(int *next, string p){
+	int len = p.length();
+	int k = -1;
+	int j = 0;
+	next[0] = -1;
+	while(j < len - 1){
+		if(k == -1 || p[j] == p [k]){
+			next[++j] = ++k;
+		}else{
+			k = next[k];
+		}
+	}
+}
+
+int KMP(string s, string p){
+	int ls = s.length();
+	int lp = p.length();
+	int si = 0;
+	int pi = 0;
+	int *next = new int[lp];
+	cal_next(next, p);
+	while(si < ls && pi < lp){
+		if(pi == -1 || s[si] == p[pi]){
+			si++;
+			pi++;
+		}else{
+			pi = next[pi];
+		}
+	}
+	delete next;
+
+	return (pi == lp)?(si - pi):-1;
+}
+
+int main(){
+	string p, s;
+	cin>>p;
+	cin>>s;
+	//p = "abacdababx";
+	//s = "swqewqrrabacdababxtooipzxcipxzc";
+	cout<<KMP(s, p)<<endl;
+	return 0;
+}
