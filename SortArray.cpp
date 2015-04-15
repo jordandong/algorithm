dc9de9146a8f71a77ed942f26b3dabdeecf09ea8
jordandong <jordandong@gmail.com>
2015-04-15 15:20:56 -0700

    Create SortArray.cpp

---
+++ SortArray.cpp
@@ -0,0 +1,29 @@
+/*
+Sort Array and make sure the Array to meet a0<=a1>=a2<=a3>=a4....
+*/
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void sortArray(vector<int> &data){
+    int sz = data.size();
+    bool up = false;  
+    for(int i = 1; i < sz; i++){
+        if((data[i] > data[i - 1]) == up)
+            swap(data[i], data[i - 1]);
+        up = !up;
+    }
+}
+
+int main(){
+    vector<int> data;
+    int N = 10;
+    for(int i = 0; i < N; i++)
+        data.push_back(i);
+    sortArray(data);
+    for(int i = 0; i < N; i++)
+        cout<<data[i];
+    cout<<endl;
+    return 1;
+}
