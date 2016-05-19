967b6aec93fc029fbdbbb6ce7adf9f37df39c6c1
J.D <jordandong@gmail.com>
2016-05-19 02:19:29 -0700

    Update LC253_MeetingRoomsII.cpp

--- LC253_MeetingRoomsII.cpp
+++ LC253_MeetingRoomsII.cpp
@@ -45,3 +45,32 @@ private:
         return interval1.start < interval2.start;
     }
 };
+
+class Solution {
+public:
+    int minMeetingRooms(vector<Interval>& intervals) {
+        vector<int> starts, ends;
+        for (const auto& i : intervals) {
+            starts.push_back(i.start);
+            ends.push_back(i.end);
+        }
+        
+        sort(starts.begin(), starts.end());
+        sort(ends.begin(), ends.end());
+        
+        int min_rooms = 0, cnt_rooms = 0;
+        int s = 0, e = 0;
+        while (s < starts.size()) {
+            if (starts[s] < ends[e]) {
+                ++cnt_rooms;  // Acquire a room.
+                // Update the min number of rooms.
+                min_rooms = max(min_rooms, cnt_rooms);
+                ++s;
+            } else {
+                --cnt_rooms;  // Release a room.
+                ++e;
+            }
+        }
+        return min_rooms;
+    }
+};
