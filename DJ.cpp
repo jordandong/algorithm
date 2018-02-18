9cc021e69eccae4ebd57b7b5d2f23807d8459e1b
J.D <jordandong@gmail.com>
2018-02-18 03:23:59 -0800

    Update DJ.cpp

--- DJ.cpp
+++ DJ.cpp
@@ -23,9 +23,16 @@ using namespace std;
  *     the algorithm takes the maximum signed long long 0x7fffffffffffffff instead
  *     -1 means infinite, unreachable
  */
- 
+
+#define DEBUG_LL_RANGE 0
 #define LMAX 0x7fffffffffffffff
-vector<long long> dijkstra(vector<vector<int>> &dG, int src) {
+
+//TIME : O(N*N*logN), SPACE : O(N)
+#if DEBUG_LL_RANGE
+vector<long long> dijkstra_v1(vector<vector<long long>> &dG, int src) {
+#else
+vector<long long> dijkstra_v1(vector<vector<int>> &dG, int src) {
+#endif
     //case 1 : null input
     //case 2 : invalid (dG.size() != dG[0].size()) input
     //case 3 : invalid source node, out of range
@@ -51,7 +58,7 @@ vector<long long> dijkstra(vector<vector<int>> &dG, int src) {
             return {};
         for (int j = 0; j < n; j++) { //O(N)
             long long dis = dG[cur_node.second][j];
-            if (cur_node.second == j || dis == -1 || vis.find(dis) != vis.end())
+            if (cur_node.second == j || dis == -1 || vis.find(j) != vis.end())
                 continue;
             if (dis < 0) //invalid input, negtive dis
                 return {};
@@ -66,18 +73,89 @@ vector<long long> dijkstra(vector<vector<int>> &dG, int src) {
     return ans;
 }
 
+ 
+//TIME : O(N*N), SPACE : O(N)
+#if DEBUG_LL_RANGE
+vector<long long> dijkstra_v2(vector<vector<long long>> &dG, int src) {
+#else
+vector<long long> dijkstra_v2(vector<vector<int>> &dG, int src) {
+#endif
+    //case 1 : null input
+    //case 2 : invalid (dG.size() != dG[0].size()) input
+    //case 3 : invalid source node, out of range
+    //case 4 : invalid distance from src to src, dG[src][src] is non zero
+    //return empty vector{}
+    int n = dG.size();
+    if (n == 0 || n != dG[0].size() || src >= n || dG[src][src] != 0)
+        return {};
+
+    vector<long long> ans(n, -1); //init as infinite to all nodes
+    unordered_map<int, bool> uvis; //map contains unvisited nodes
+    unordered_map<int, bool> vis; //map contains visited nodes
+    
+    uvis[src] = true;
+    ans[src] = 0; //src to src is zero
+    while (!uvis.empty()) { //O(N)
+        long long mi = LMAX;
+        int cur_node = -1;
+        for (int i = 0; i < n; i++) { //O(N)
+            if (uvis.find(i) == uvis.end())
+                continue;
+            if (ans[i] <= mi) {
+                mi = ans[i];
+                cur_node = i;
+            }
+        }
+
+        uvis.erase(cur_node);
+        vis[cur_node] = true;
+        if (dG[cur_node][cur_node] != 0)
+            return {};
+        for (int j = 0; j < n; j++) { //O(N)
+            long long dis = dG[cur_node][j];
+            if (cur_node == j || dis == -1 || vis.find(j) != vis.end())
+                continue;
+            if (dis < 0) //invalid input, negtive dis
+                return {};
+            unsigned long long new_dis = ans[cur_node] + dis;
+            if (ans[j] == -1 ||  new_dis < ans[j]) {
+                uvis[j] = true;
+                ans[j] = min(new_dis, (unsigned long long)LMAX);
+            }
+        }
+    }
+    return ans;
+}
+
 int main()
 {
+    vector<long long> ans;
+#if not DEBUG_LL_RANGE
     vector<vector<int>> dG = {};
-    vector<long long> ans = dijkstra(dG, 0);
+    ans = dijkstra_v1(dG, 0);
     cout<<"case 1 null input, ans size: "<<ans.size()<<endl;
     for (auto e : ans) {
         cout<<e<<" ";
     }
     cout<<endl;
     
+    ans = dijkstra_v2(dG, 0);
+    cout<<"case 1 null input, ans size: "<<ans.size()<<endl;
+    for (auto e : ans) {
+        cout<<e<<" ";
+    }
+    cout<<endl;
+    
+    
     dG = {{0, 2}, {1, 2}, {2, 3}};
-    ans = dijkstra(dG, 0);
+    ans = dijkstra_v1(dG, 0);
+    cout<<"case 2 input size mismatch, ans size: "<<ans.size()<<endl;
+    for (auto e : ans) {
+        cout<<e<<" ";
+    }
+    cout<<endl;
+    
+    ans = dijkstra_v2(dG, 0);
     cout<<"case 2 input size mismatch, ans size: "<<ans.size()<<endl;
     for (auto e : ans) {
         cout<<e<<" ";
@@ -85,7 +163,14 @@ int main()
     cout<<endl;
     
     dG = {{0, 2}, {1, 2}};
-    ans = dijkstra(dG, 3);
+    ans = dijkstra_v1(dG, 3);
+    cout<<"case 3 invalid source, ans size: "<<ans.size()<<endl;
+    for (auto e : ans) {
+        cout<<e<<" ";
+    }
+    cout<<endl;
+    
+    ans = dijkstra_v2(dG, 3);
     cout<<"case 3 invalid source, ans size: "<<ans.size()<<endl;
     for (auto e : ans) {
         cout<<e<<" ";
@@ -93,7 +178,14 @@ int main()
     cout<<endl;
 
     dG = {{1, 2}, {1, 2}};
-    ans = dijkstra(dG, 0);
+    ans = dijkstra_v1(dG, 0);
+    cout<<"case 4 invalid src to src, ans size: "<<ans.size()<<endl;
+    for (auto e : ans) {
+        cout<<e<<" ";
+    }
+    cout<<endl;
+    
+    ans = dijkstra_v2(dG, 0);
     cout<<"case 4 invalid src to src, ans size: "<<ans.size()<<endl;
     for (auto e : ans) {
         cout<<e<<" ";
@@ -101,7 +193,14 @@ int main()
     cout<<endl;
     
     dG = {{0, 2}, {0, 1}};
-    ans = dijkstra(dG, 0);
+    ans = dijkstra_v1(dG, 0);
+    cout<<"case 5 invalid nodex to nodex, ans size: "<<ans.size()<<endl;
+    for (auto e : ans) {
+        cout<<e<<" ";
+    }
+    cout<<endl;
+    
+    ans = dijkstra_v2(dG, 0);
     cout<<"case 5 invalid nodex to nodex, ans size: "<<ans.size()<<endl;
     for (auto e : ans) {
         cout<<e<<" ";
@@ -109,7 +208,14 @@ int main()
     cout<<endl;
     
     dG = {{0, 1, 10, 10}, {0, 0, 2, 8}, {0, 0, 0, -2}, {0, 0, 0, 0}};
-    ans = dijkstra(dG, 0);
+    ans = dijkstra_v1(dG, 0);
+    cout<<"case 6 invalid negtive input, ans size: "<<ans.size()<<endl;
+    for (auto e : ans) {
+        cout<<e<<" ";
+    }
+    cout<<endl;
+    
+    ans = dijkstra_v2(dG, 0);
     cout<<"case 6 invalid negtive input, ans size: "<<ans.size()<<endl;
     for (auto e : ans) {
         cout<<e<<" ";
@@ -117,15 +223,29 @@ int main()
     cout<<endl;
     
     dG = {{0, 1, 10, 10}, {0, 0, 2, 8}, {0, 0, 0, -1}, {0, 0, 0, 0}};
-    ans = dijkstra(dG, 0);
-    cout<<"case 7 normal input, ans size: "<<ans.size()<<endl;
+    ans = dijkstra_v1(dG, 0);
+    cout<<"case 7 normal input with -1, ans size: "<<ans.size()<<endl;
+    for (auto e : ans) {
+        cout<<e<<" ";
+    }
+    cout<<endl;
+
+    ans = dijkstra_v2(dG, 0);
+    cout<<"case 7 normal input with -1, ans size: "<<ans.size()<<endl;
     for (auto e : ans) {
         cout<<e<<" ";
     }
     cout<<endl;
 
     dG = {{0, 1, 10, 10}, {0, 0, 2, 8}, {0, 0, 0, 5}, {0, 0, 0, 0}};
-    ans = dijkstra(dG, 0);
+    ans = dijkstra_v1(dG, 0);
+    cout<<"case 8 normal input, ans size: "<<ans.size()<<endl;
+    for (auto e : ans) {
+        cout<<e<<" ";
+    }
+    cout<<endl;
+    
+    ans = dijkstra_v2(dG, 0);
     cout<<"case 8 normal input, ans size: "<<ans.size()<<endl;
     for (auto e : ans) {
         cout<<e<<" ";
@@ -134,15 +254,36 @@ int main()
     
     
     dG = {{0, 1, -1, -1}, {2, 0, -1, -1}, {-1, -1, 0, 5}, {-1, -1, 6, 0}};
-    ans = dijkstra(dG, 0);
+    ans = dijkstra_v1(dG, 0);
     cout<<"case 9 normal input seperated graph, ans size: "<<ans.size()<<endl;
     for (auto e : ans) {
         cout<<e<<" ";
     }
     cout<<endl;
     
+    ans = dijkstra_v2(dG, 0);
+    cout<<"case 9 normal input seperated graph, ans size: "<<ans.size()<<endl;
+    for (auto e : ans) {
+        cout<<e<<" ";
+    }
+    cout<<endl;
+
+#else
+    vector<vector<long long>> dG = {{0, 0x7fffffffffffffff, -1}, {0, 0, 0x7fffffffffffffff}, {0, 0x7fffffffffffffff, 0}};
+    ans = dijkstra_v1(dG, 0);
+    cout<<"case 10 out of range input, ans size: "<<ans.size()<<endl;
+    for (auto e : ans) {
+        cout<<e<<" ";
+    }
+    cout<<endl;
+    
+    ans = dijkstra_v2(dG, 0);
+    cout<<"case 10 out of range input, ans size: "<<ans.size()<<endl;
+    for (auto e : ans) {
+        cout<<e<<" ";
+    }
+    cout<<endl;
+#endif
+    
     return 0;
 }
-
-
-
